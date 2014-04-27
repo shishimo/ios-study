09813483b97b53c8bcde5e7fc01b5d88341d7b5c
shishimo <my_blue_wish@yahoo.co.jp>
2014-04-27 13:41:59 +0900

    Initial Commit

---
+++ ViewDeck/ViewDeck/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ViewDeck
+//
+//  Created by shishimo on 2014/04/27.
+//  Copyright (c) 2014年 shishimo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ViewDeck/ViewDeck/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ViewDeck
+//
+//  Created by shishimo on 2014/04/27.
+//  Copyright (c) 2014年 shishimo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
